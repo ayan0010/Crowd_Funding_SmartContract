87a55a5cbbc5376edd9bcaf2edcb26279a5472fb
itzam10 <resoayan@gmail.com>
2022-10-06 18:08:12 +0530

    smartcontract done

---
+++ CrowdFunding.sol
@@ -0,0 +1,90 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract CrowdFunding{
+    mapping(address=>uint) public contributors;
+    address public manager;
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+
+    struct Request{
+         string description;
+         address payable recipient;
+         uint value;
+         bool completed; // checking if payment is done
+         uint noOfVoters;
+         mapping(address=>bool) voters;
+    }
+
+    mapping(uint=>Request) public request;
+    uint public numRequests;
+
+    constructor(uint _target,uint _deadline){
+        target=_target;
+        deadline=block.timestamp+_deadline;
+        minimumContribution=100 wei;
+        manager=msg.sender;
+    }
+  
+  function sendEth() public payable {
+      require(block.timestamp < deadline,"Deadline has passed");
+      require(msg.value >= minimumContribution,"Minimum Contribution is not met");
+      if(contributors[msg.sender]==0){
+          noOfContributors++;
+      }
+      contributors[msg.sender]+=msg.value;
+      raisedAmount+=msg.value;  
+  }
+
+  function getContractBalance() public view returns(uint){
+      return address(this).balance;
+  }
+
+
+  function refund() public {
+      require(block.timestamp>deadline && raisedAmount<target,"You are not eligible for refund");
+      require(contributors[msg.sender]>0);
+      address payable user = payable(msg.sender);
+      user.transfer(contributors[msg.sender]);
+      contributors[msg.sender]=0;
+
+  }
+
+    modifier onlyManager{
+        require(msg.sender==manager,"Only Manager can call this function");
+        _;
+    }
+
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyManager{
+          Request storage newRequest = request[numRequests]; // storage cause we used mappping in struct Request
+          numRequests++;
+          newRequest.description=_description;
+          newRequest.recipient=_recipient;
+          newRequest.value=_value;
+          newRequest.completed=false;
+          newRequest.noOfVoters=0;
+    }
+
+
+    function voteRequest(uint _requestNo) public {
+       require(contributors[msg.sender]>0,"You must be a contributor");
+       Request storage thisRequest = request[_requestNo];
+       require(thisRequest.voters[msg.sender]==false,"You are already Voted");
+       thisRequest.voters[msg.sender]=true;
+       thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint _requestNo) public onlyManager{
+       require(raisedAmount>=target);
+       Request storage thisRequest = request[_requestNo];
+       require(thisRequest.completed==false,"The request has been completed");
+       require(thisRequest.noOfVoters>noOfContributors/2,"Majority does not support");
+       thisRequest.recipient.transfer(thisRequest.value);
+       thisRequest.completed=true;
+    }
+
+
+}
